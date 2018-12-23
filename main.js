2e0e5f02b77df2316ead93ce24446cabef300c13
mcoons <mcoons67@gmail.com>
2018-12-23 15:32:51 -0700

    added options and styling

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ let ctx = canvas.getContext("2d");
 ctx.globalAlpha = 0;
 
 document.body.setAttribute("onresize","resizeCanvas()");
+window.addEventListener('keydown',
+  function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='number'){e.preventDefault();return false;}}},true);
 
 download_img = function(el) {
-  // var image = canvas.toDataURL("image/png");
   el.href = canvas.toDataURL("image/png");
   el.download = "mypainting.png"
 };
@@ -25,16 +26,16 @@ let wide = width > height ? true : false;
 
 let colorTheta = 1530;
 let index = 0;
-let inc = 79717;
-let inc2 = 79717;
+let inc = inc2 = 1000;
+let rot = rot2 = 0
 let zoom = 1;
 var changed = true;
 var backgroundChanged = true;
 
 let x,y,r;
 let scale = wide ? halfHeight/8 : halfWidth/8;
-var a=2,b=7,c=929;
-var a2=2,b2=7,c2=929;
+var a=2,b=4,c=6;
+var a2=5,b2=2.5,c2=6;
 
 let backgroundInterval = null;
 let parainterval = null;
@@ -91,7 +92,7 @@ function setCanvas(){
 //////////////////////////////////////////////////////////////////////
 
 function refreshImages(){
-  parametric(inc);
+  parametric();
   if (backgroundChanged) {
     backgroundChanged = false;
     clearInterval(backgroundInterval);
@@ -115,78 +116,105 @@ function refreshImages(){
 
 //////////////////////////////////////////////////////////////////////
 
-function parametric(inc){
+function parametric(){
   // ctx.fillStyle = "#FFFFFF";
 
+  // First equation variables
   var aslider = document.getElementById('aValue');
   var atext = document.getElementById('aText');
+  
   aslider.value = a;
-  aslider.oninput = function(){atext.innerText = "Value of A: " + aslider.value; update()}
-  atext.innerText = "Value of A: " + aslider.value;
+  aslider.oninput = function(){atext.value = aslider.value; update()}
+  atext.value = aslider.value;
+  atext.oninput = function(){aslider.value = atext.value; update()}
 
   var bslider = document.getElementById('bValue');
   var btext = document.getElementById('bText');
+
   bslider.value = b;
-  bslider.oninput = function(){btext.innerText = "Value of B: " + bslider.value; update()}
-  btext.innerText = "Value of B: " + bslider.value;
+  bslider.oninput = function(){btext.value = bslider.value; update()}
+  btext.value = bslider.value;
+  btext.oninput = function(){bslider.value = btext.value; update()}
 
   var cslider = document.getElementById('cValue');
   var ctext = document.getElementById('cText');
+
   cslider.value = c;
-  cslider.oninput = function(){ctext.innerText = "Value of C: " + cslider.value; update()}
-  ctext.innerText = "Value of C: " + cslider.value;
+  cslider.oninput = function(){ctext.value = cslider.value; update()}
+  ctext.value = cslider.value;
+  ctext.oninput = function(){cslider.value = ctext.value; update()}
+
+  var rslider = document.getElementById('rValue');
+  var rtext = document.getElementById('rText');
+
+  rslider.value = rot;
+  rslider.oninput = function(){rtext.value = rslider.value; update()}
+  rtext.value = rslider.value;
+  rtext.oninput = function(){rslider.value = rtext.value; update()}
+
+
 
   var islider = document.getElementById('iValue');
   var itext = document.getElementById('iText');
+
   islider.value = inc;
-  islider.oninput = function(){itext.innerText = "Value of theta step: (" + islider.value + ") " + Math.PI/islider.value; update()}
-  itext.innerText = "Value of theta step: (" + islider.value + ") " + Math.PI/islider.value;
+  islider.oninput = function(){itext.value = islider.value; update()}
+  itext.value = islider.value;
+  itext.oninput = function(){islider.value = itext.value; update()}
 
 
-
+  // Second equation variables
   var aslider2 = document.getElementById('aValue2');
   var atext2 = document.getElementById('aText2');
+
   aslider2.value = a2;
-  aslider2.oninput = function(){atext2.innerText = "Value of A: " + aslider2.value; update()}
-  atext2.innerText = "Value of A: " + aslider2.value;
+  aslider2.oninput = function(){atext2.value = aslider2.value; update()}
+  atext2.value = aslider2.value;
+  atext2.oninput = function(){aslider2.value = atext2.value; update()}
 
   var bslider2 = document.getElementById('bValue2');
   var btext2 = document.getElementById('bText2');
   bslider2.value = b2;
-  bslider2.oninput = function(){btext2.innerText = "Value of B: " + bslider2.value; update()}
-  btext2.innerText = "Value of B: " + bslider2.value;
+  bslider2.oninput = function(){btext2.value = bslider2.value; update()}
+  btext2.value = bslider2.value;
+  btext2.oninput = function(){bslider2.value = btext2.value; update()}
 
   var cslider2 = document.getElementById('cValue2');
   var ctext2 = document.getElementById('cText2');
   cslider2.value = c2;
-  cslider2.oninput = function(){ctext2.innerText = "Value of C: " + cslider2.value; update()}
-  ctext2.innerText = "Value of C: " + cslider2.value;
+  cslider2.oninput = function(){ctext2.value = cslider2.value; update()}
+  ctext2.value = cslider2.value;
+  ctext2.oninput = function(){cslider2.value = ctext2.value; update()}
+
+
+  var rslider2 = document.getElementById('rValue2');
+  var rtext2 = document.getElementById('rText2');
+
+  rslider2.value = rot;
+  rslider2.oninput = function(){rtext2.value = rslider2.value; update()}
+  rtext2.value = rslider2.value;
+  rtext2.oninput = function(){rslider2.value = rtext2.value; update()}
+
+
 
   var islider2 = document.getElementById('iValue2');
   var itext2 = document.getElementById('iText2');
   islider2.value = inc2;
-  islider2.oninput = function(){itext2.innerText = "Value of theta step: (" + islider2.value + ") " + Math.PI/islider2.value; update()}
-  itext2.innerText = "Value of theta step: (" + islider2.value + ") " + Math.PI/islider.value;
+  islider2.oninput = function(){itext2.value = islider2.value; update()}
+  itext2.value = islider2.value;
+  itext2.oninput = function(){islider2.value = itext2.value; update()}
 
 
 
-
-
-
-
-
-
-
-
-
-
-  
+  // Zoom option
   var zslider = document.getElementById('zValue');
   var ztext = document.getElementById('zText');
   zslider.value = zoom*10;
   zslider.oninput = function(){ztext.innerText = "Value of zoom: " + zslider.value/10; update()}
   ztext.innerText = "Value of zoom: " + zslider.value/10;
 
+
+  // Background options
   var coloredRadio = document.getElementById('colored');
   coloredRadio.oninput = function(){console.log("colored radio");backgroundType = "colored"; backgroundChanged=true;refreshImages();}
   var blackRadio = document.getElementById('black');
@@ -197,10 +225,12 @@ function parametric(inc){
   function update(){  a=Number(aslider.value); 
                       b=Number(bslider.value); 
                       c=Number(cslider.value); 
+                      rot=Number(rslider.value);
                       inc = Number(islider.value);
                       a2=Number(aslider2.value); 
                       b2=Number(bslider2.value); 
                       c2=Number(cslider2.value); 
+                      rot2=Number(rslider2.value);
                       inc2 = Number(islider2.value);
                       zoom = Number(zslider.value/10);
                       changed = true;
@@ -208,7 +238,7 @@ function parametric(inc){
 
 
   clearInterval(parainterval);
-  parainterval = setInterval( drawParametric, 300);
+  parainterval = setInterval( drawParametric, 10);
 
   function drawParametric(){
     if (!changed) return;
@@ -222,16 +252,17 @@ function parametric(inc){
     ctx.scale(zoom,zoom);
 
     for (let theta = 0; theta <= 2*Math.PI; theta+=Math.PI/inc){
-      r = (a+b*Math.sin(c*theta));
+      r = (a+b*Math.sin(c*(theta+rot)));
+      // r = (a+b*Math.sin(c*theta));
       ctx.fillStyle = palette[(Math.abs(Math.round(265*r)+colorTheta)%1530)].color;   
       x = Math.cos(theta)*r*scale;
       y = Math.sin(theta)*r*scale;
-      drawPoint(ctx, x, y, 1);
+      drawPoint(ctx, x, y, 2);
     }
 
 
     for (let theta = 0; theta <= 2*Math.PI; theta+=Math.PI/inc2){
-      r = (a2+b2*Math.sin(c2*theta));
+      r = (a2+b2*Math.sin(c2*(theta+rot2)));
       ctx.fillStyle = palette[(Math.abs(Math.round(265*r)+colorTheta)%1530)].color;   
       x = Math.cos(theta)*r*scale;
       y = Math.sin(theta)*r*scale;
@@ -344,7 +375,7 @@ function buildPalette(){
 
 function scrollingBackground(){
 
-  let interval = setInterval(scrollColors, 20, backgroundCtx, -.75);
+  let interval = setInterval(scrollColors, 20, backgroundCtx, -.55);
   return interval;
 
   function scrollColors(ctx, percent){
